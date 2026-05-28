7346b149eef9ccf65a236391a054a853757a6b5e
Lily <haquynhchi@gmail.com>
2026-05-28 14:40:38 +1000

    drop species column in plants table

---
+++ src/prisma/migrations/20260528043920_drop_species_column/migration.sql
@@ -0,0 +1,9 @@
+/*
+ Warnings:
+
+ - You are about to drop the column `species` on the `plants` table. All the data in the column will be lost.
+ */
+-- AlterTable
+ALTER TABLE "plants"
+  DROP COLUMN "species";
+
